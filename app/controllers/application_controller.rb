6eb64d46c1dd5fcb110152e22e8f2234a01a9d5a
torabul <rstoyo.hiroto.takeda@gmail.com>
2023-12-08 17:28:01 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
